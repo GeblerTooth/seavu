6b6e43d51820a0a21602a8d8de708cbbbfba4f28
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-18 20:36:29 +0000

    working auth sys

--- inventory/models.py
+++ inventory/models.py
@@ -1,27 +1,17 @@
 from django.db import models
-from django.core.validators import EmailValidator
-from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
 
 from datetime import date
 
 class Employee(models.Model):
-    email_regex = EmailValidator()
-    email = models.CharField(validators=[email_regex], max_length=256, primary_key=True)
-    firstname = models.CharField(max_length=128)
-    surname = models.CharField(max_length=128)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     occupation = models.PositiveSmallIntegerField(choices={0: "Full", 1: "Part", 2: "Contractor", 3: "Volunteer", 4: "Other"})
-    department = models.CharField(max_length=256, blank=True, null=True)
-    address_1 = models.CharField(max_length=128, blank=True, null=True)
-    address_2 = models.CharField(max_length=128, blank=True, null=True)
-    address_3 = models.CharField(max_length=128, blank=True, null=True)
-    address_postcode = models.CharField(max_length=16, blank=True, null=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+44 203 048 4377'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=16, blank=True, null=True)
+    department = models.CharField(max_length=128, blank=True, null=True)
 
 class Computer(models.Model):
     name = models.CharField(max_length=15, help_text="System name or other.") # Max Length for Windows System Name is 15 chars.
     status = models.PositiveSmallIntegerField(choices={0: "Active", 1: "Inactive", 2: "Awaiting return", 3: "In transit", 4: "Awaiting repairs", 5: "Lost", 6: "Stolen", 7: "Quarantined", 8: "Unknown"})
-    user = models.ForeignKey("Employee", on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     location = models.CharField(max_length=256, blank=True, null=True)
     make = models.CharField(max_length=256)
     model = models.CharField(max_length=256)
@@ -40,6 +30,6 @@ class Software(models.Model):
     company_requisite = models.BooleanField()
     has_license = models.BooleanField()
 
-class ComputerSoftware(models.Model):
+class ComputerSoftware(models.Model): # Join table for Computer and Software
     computer_id = models.ForeignKey("Computer", on_delete=models.CASCADE)
     software_id = models.ForeignKey("Software", on_delete=models.CASCADE)
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
+from .forms import UserRegisterForm, EmployeeRegisterForm
 from .models import Computer
 
+@login_required()
 def index(request):
     computer_list = Computer.objects.all()
     return render(request, "inventory/index.html", {"computer_list": computer_list})
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        user = authenticate(request, username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Username or password is incorrect.")
+    elif request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = AuthenticationForm()
+        return render(request, "registration/login.html", {"form": form})
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegisterForm(request.POST)
+        employee_form = EmployeeRegisterForm(request.POST)
+        if user_form.is_valid() and employee_form.is_valid():
+            # Automatically asign the user to the employee.
+            user_instance = user_form.save(commit=False) # Get user instance by saving a non-commit.
+            employee_form.instance.user = user_instance # Set user instance as employee foreign key.
+            user_form.save()
+            employee_form.save()
+    else:
+        user_form = UserRegisterForm()
+        employee_form = EmployeeRegisterForm()
+    return render(request, 'registration/register.html', {'user_form': user_form, 'employee_form': employee_form})
