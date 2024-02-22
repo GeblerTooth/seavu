0622d0ad95bdf835b6c7059bb2e87bb9c4526399
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-22 21:07:17 +0000

    update session engine

--- seavu/settings.py
+++ seavu/settings.py
@@ -136,6 +136,7 @@ else:
         }
     }
 
+SESSION_ENGINE= 'django.contrib.sessions.backends.cached_db'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
