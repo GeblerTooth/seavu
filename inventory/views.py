ef383f2df78b2c98b8f5b1c3039f40e81fdfe958
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-22 13:47:02 +0000

    computer crud views + login req

--- inventory/views.py
+++ inventory/views.py
@@ -2,22 +2,49 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate
+from django.forms.models import model_to_dict
 from django.http import HttpResponse
 
-from .forms import UserRegisterForm, EmployeeRegisterForm
+from .forms import UserRegisterForm, EmployeeRegisterForm, ComputerForm
 from .models import Computer
 
 @login_required()
 def index(request):
-    computer_list = Computer.objects.all()
-    return render(request, "inventory/index.html", {"computer_list": computer_list})
+    return render(request, "inventory/index.html")
 
+@login_required()
 def inventory(request):
-    return HttpResponse("Hello from Inventory.")
+    computer_list = Computer.objects.all()
+    return render(request, "inventory/inventory.html", {"computer_list": computer_list})
 
+@login_required()
+def delete_computer(request, computer_id):
+    computer = Computer.objects.get(id=computer_id)
+    computer.delete()
+    return redirect('inventory')
+
+@login_required()
+def inspect_computer(request, computer_id):
+    import sys
+    computer = model_to_dict(Computer.objects.get(id=computer_id))
+    print(str(computer), file=sys.stderr)
+    return render(request, "inventory/inspect_computer.html", {"computer": computer})
+
+@login_required()
+def add_computer(request):
+    if request.method == "POST":
+        form = ComputerForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ComputerForm()
+    return render(request, "inventory/add_computer.html", {"form": form})
+
+@login_required()
 def software(request):
     return HttpResponse("Hello from Software.")
 
+@login_required()
 def licences(request):
     return HttpResponse("Hello from Licences.")
 
