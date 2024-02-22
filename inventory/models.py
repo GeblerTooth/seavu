53f41d53a74be1f40cd4b5e364699cde4dbb7723
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-22 19:14:02 +0000

    change models to show chars

--- inventory/models.py
+++ inventory/models.py
@@ -5,19 +5,19 @@ from datetime import date
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    occupation = models.PositiveSmallIntegerField(choices={0: "Full", 1: "Part", 2: "Contractor", 3: "Volunteer", 4: "Other"}, blank=True, null=True)
+    occupation = models.CharField(max_length=128, choices={"Full": "Full", "Part": "Part", "Contractor": "Contractor", "Volunteer": "Volunteer", "Other": "Other"}, blank=True, null=True)
     department = models.CharField(max_length=128, blank=True, null=True)
 
 class Computer(models.Model):
     name = models.CharField(max_length=15, help_text="System name or other.") # Max Length for Windows System Name is 15 chars.
-    status = models.PositiveSmallIntegerField(choices={0: "Active", 1: "Inactive", 2: "Awaiting return", 3: "In transit", 4: "Awaiting repairs", 5: "Lost", 6: "Stolen", 7: "Quarantined", 8: "Unknown"})
+    status = models.CharField(max_length=128, choices={"Active": "Active", "Inactive": "Inactive", "Awaiting return": "Awaiting return", "In transit": "In transit", "Awaiting repairs": "Awaiting repairs", "Lost": "Lost", "Stolen": "Stolen", "Quarantined": "Quarantined", "Unknown": "Unknown"})
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     location = models.CharField(max_length=256, blank=True, null=True)
     make = models.CharField(max_length=256)
     model = models.CharField(max_length=256)
     serial_number = models.CharField(max_length=128, blank=True, null=True)
     tag_number = models.CharField(max_length=128, blank=True, null=True)
-    category = models.PositiveSmallIntegerField(choices={0: "PC", 1: "Laptop", 2: "Mini PC", 3: "SBC", 4: "All-in-One PC", 5: "Server", 6: "NAS", 7: "Other"})
+    category = models.CharField(max_length=128, choices={"PC": "PC", "Laptop": "Laptop", "Mini PC": "Mini PC", "SBC": "SBC", "All-in-One PC": "All-in-One PC", "Server": "Server", "NAS": "NAS", "Other": "Other"})
     date_of_purchase = models.DateField(default=date.today, blank=True, null=True)
     memory_gb = models.PositiveIntegerField(blank=True, null=True)
     storage_gb = models.PositiveIntegerField(blank=True, null=True)
