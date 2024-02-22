e43bdad5e007e9fd5bd120959f16ad2a4d723b96
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-22 20:32:18 +0000

    change inventory config

--- seavu/settings.py
+++ seavu/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "inventory",
+    "inventory.apps.InventoryConfig",
 ]
 
 MIDDLEWARE = [
@@ -193,5 +193,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
-
-DEBUG = True
