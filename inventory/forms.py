e246298c79ab834054579dd4f14dfaec8bc772b8
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-22 15:04:27 +0000

    add software form

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Employee, Computer
+from .models import Employee, Computer, Software
 
 class UserRegisterForm(UserCreationForm):
 
@@ -19,4 +19,10 @@ class ComputerForm(forms.ModelForm):
     
     class Meta:
         model = Computer
+        fields = '__all__'
+
+class SoftwareForm(forms.ModelForm):
+    
+    class Meta:
+        model = Software
         fields = '__all__'
