5e2d04b2c1ba50eb5ad1a771a7848852a6ccd700
Tristan Golding <Tristan@geblertooth.co.uk>
2024-02-29 20:37:39 +0000

    model edition for manytomanyfield for computersoftware

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Employee, Computer, Software, ComputerSoftware
+from .models import Employee, Computer, Software
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -21,4 +21,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register({Computer, Software, ComputerSoftware})
+admin.site.register({Computer, Software})
--- inventory/models.py
+++ inventory/models.py
@@ -8,6 +8,14 @@ class Employee(models.Model):
     occupation = models.CharField(max_length=128, choices={"Full": "Full", "Part": "Part", "Contractor": "Contractor", "Volunteer": "Volunteer", "Other": "Other"}, blank=True, null=True)
     department = models.CharField(max_length=128, blank=True, null=True)
 
+class Software(models.Model):
+    name = models.CharField(max_length=256)
+    company_requisite = models.BooleanField()
+    has_licence = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+
 class Computer(models.Model):
     name = models.CharField(max_length=15, help_text="System name or other.") # Max Length for Windows System Name is 15 chars.
     status = models.CharField(max_length=128, choices={"Active": "Active", "Inactive": "Inactive", "Awaiting return": "Awaiting return", "In transit": "In transit", "Awaiting repairs": "Awaiting repairs", "Lost": "Lost", "Stolen": "Stolen", "Quarantined": "Quarantined", "Unknown": "Unknown"})
@@ -15,6 +23,7 @@ class Computer(models.Model):
     location = models.CharField(max_length=256, blank=True, null=True)
     make = models.CharField(max_length=256)
     model = models.CharField(max_length=256)
+    software = models.ManyToManyField(Software)
     serial_number = models.CharField(max_length=128, blank=True, null=True)
     tag_number = models.CharField(max_length=128, blank=True, null=True)
     category = models.CharField(max_length=128, choices={"PC": "PC", "Laptop": "Laptop", "Mini PC": "Mini PC", "SBC": "SBC", "All-in-One PC": "All-in-One PC", "Server": "Server", "NAS": "NAS", "Other": "Other"})
@@ -25,11 +34,5 @@ class Computer(models.Model):
     clock_speed_ghz = models.FloatField(blank=True, null=True)
     gpu = models.CharField(max_length=256, blank=True, null=True)
 
-class Software(models.Model):
-    name = models.CharField(max_length=256)
-    company_requisite = models.BooleanField()
-    has_licence = models.BooleanField()
-
-class ComputerSoftware(models.Model): # Join table for Computer and Software.
-    computer_id = models.ForeignKey("Computer", on_delete=models.CASCADE)
-    software_id = models.ForeignKey("Software", on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
